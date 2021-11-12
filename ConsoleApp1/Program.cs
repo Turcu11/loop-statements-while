21e7adfc3b4aaeab192609bffa5f9dcfa2368b94
Iosif Turcu <iosif.turcu@emmanuel.ro>
2021-11-12 08:50:35 +0200

    Functional 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretUser = "admin";
+            string secretPassword = "12345";
+
+            Console.WriteLine("Please log in.");
+            bool isAuthentificated = false;
+
+            while(!isAuthentificated)
+            {
+                Console.Write("User: ");
+                string user = Console.ReadLine();
+                Console.Write("Password: ");
+                string password = Console.ReadLine();
+
+                if(string.Equals(user, secretUser, StringComparison.OrdinalIgnoreCase) &&
+                    string.Equals(password, secretPassword, StringComparison.Ordinal))
+                {
+                    Console.WriteLine($"Welcome {user} !");
+                    isAuthentificated = true;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong. Try again.");
+                }
+            }
+        }
+    }
+}
